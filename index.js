1a9570490142b213c08b4c9cc85de380c2163f50
rdevcharles <rondell.charles@generalassemb.ly>
2023-04-29 16:32:23 -0400

    create a read profiles

---
+++ index.js
@@ -0,0 +1,14 @@
+// import { createRoot } from 'react-dom/client';
+
+
+
+
+// // Clear the existing HTML content
+// document.body.innerHTML = '<div id=app></div>';
+// console.log("hello")
+// // Render your React component instead
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello, world</h1>);
+
+
+alert("hello")
--- js/profile_creation.js
+++ js/profile_creation.js
@@ -1,18 +1,47 @@
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
+const { createElement } = require("react");
 const saltRounds = 10;
-const myPlaintextPassword = "password";
+
 const prisma = new PrismaClient();
 
+
+
+
+async function get_profiles() {
+  let user_profile;
+  let user_profiles = await prisma.user.findMany();
+  
+  //display profiles
+  
+  let user_profile_list = document.getElementById("profile_ul");
+  for (user_profile of user_profiles) {
+    let user_profile_a = document.createElement("a");
+    let user_profile_li = document.createElement("li");
+    user_profile_li.textContent = `${user_profile.name}`;
+    user_profile_a.appendChild(user_profile_li);
+    user_profile_a.href = `${user_profile.id}`;
+    user_profile_list.appendChild(user_profile_a);
+  }
+}
+
+get_profiles();
+
+const user_profile_name = document.getElementById("user_profile_name");
+const user_profile_email = document.getElementById("user_profile_email");
+const user_passphrase = document.getElementById("user_passphrase");
+
+
 async function create_new_profile() {
- 
-  bcrypt.hash(myPlaintextPassword, saltRounds, async function (err, hash) {
-      try {
-       
+  user_profile_name.innerText = user_profile_name.value
+  user_profile_email.innerText = user_profile_email.value
+  user_passphrase.innerText = user_passphrase.value
+  bcrypt.hash(user_passphrase.value, saltRounds, async function (err, hash) {
+    try {
       await prisma.user.create({
         data: {
-          name: "name",
-          email: "email",
+          name: user_profile_name.value,
+          email: user_profile_email.value,
           passphrase: hash,
         },
       });
@@ -20,9 +49,18 @@ async function create_new_profile() {
       alert(error);
     }
   });
-}
 
+}
 
 const create_button = document.getElementById("create");
 
 create_button.addEventListener("click", create_new_profile);
+
+
+// function test() {
+//   user_profile_name.innerText = user_profile_name.value
+ 
+//   console.log(user_profile_name.value);
+// }
+
+// user_profile_name.addEventListener("change", test)
